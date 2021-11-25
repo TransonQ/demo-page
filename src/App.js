16f38f16aacb055065bf006145f6c9975e2dee51
quanscheng <quanscheng@163.com>
2021-11-25 15:14:07 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-
 import React, { useCallback, useState } from "react"
 import { Card, Page, Tabs } from "@shopify/polaris"
 import SyncRecoilAtom from "./Pages/page1"
-import AsyncData1 from "./Pages/page2"
-import AsyncData2 from "./Pages/page3"
 
 function App() {
   // tabsContents顺序 === pages顺序
   let tabContents = ["时间选择器"]
-  let pages = [<SyncRecoilAtom />, <AsyncData1 />, <AsyncData2 />]
+  let pages = [<SyncRecoilAtom />]
 
   const tabs = tabContents.map(tab => {
     return {
@@ -19,18 +16,13 @@ function App() {
   })
   // tabs选择处理函数和状态
   const [selected, setSelected] = useState(0)
-  const handleTabChange = useCallback(
-    selectedTabIndex => setSelected(selectedTabIndex),
-    []
-  )
+  const handleTabChange = useCallback(selectedTabIndex => setSelected(selectedTabIndex), [])
 
   return (
     <Page title='example' divider narrowWidth>
       <Card>
         <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
-          <Card.Section title={tabs[selected].content + "内容如下:"}>
-            {pages[selected]}
-          </Card.Section>
+          <Card.Section title={tabs[selected].content + "内容如下:"}>{pages[selected]}</Card.Section>
         </Tabs>
       </Card>
     </Page>
