a887f984a4e54773b023371c94d465a66df2e410
quanscheng <quanscheng@163.com>
2021-12-20 14:28:06 +0800

    解决了中文兼容的问题

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { jsPDF } from "jspdf"
+import 'jspdf-autotable'
+import './NotoSansCJKtc-Regular-normal'
+
 function App() {
-  return <div className='App'>hello</div>
+  const download = () => {
+    const doc = new jsPDF()
+    doc.setFont("NotoSansCJKtc-Regular");
+    doc.text("Hello world! 中文付款清单", 30, 10)
+    doc.save("a4.pdf")
+    // doc.autoPrint();
+  }
+
+  return (
+    <>
+      <div id='demo'>
+        <h1>hello</h1>
+      </div>
+      <button onClick={download}>下载</button>
+    </>
+  )
 }
 
 export default App
